9dd14b05f2eb796dff036b9b1f5443d624241409
cyberfrogg <alexandersudnik.q@gmail.com>
2023-05-04 22:56:31 +0300

    MF - locking buttons

--- Assets/Scripts/Game/Ui/PerformAction/PerformActionController.cs
+++ Assets/Scripts/Game/Ui/PerformAction/PerformActionController.cs
@@ -8,16 +8,22 @@ namespace Game.Ui.PerformAction
     public class PerformActionController : UiController<PerformActionView>, IUiInitializable
     {
         private readonly ActionContext _action;
+        private readonly GameContext _game;
 
         public PerformActionController(
-            ActionContext action
+            ActionContext action,
+            GameContext game
             )
         {
             _action = action;
+            _game = game;
         }
 
         public void Initialize()
         {
+            var cat = _game.CatEntity;
+            View.Link(cat, _game);
+            
             foreach (var button in View.Buttons)
             {
                 button.Button.OnClickAsObservable().Subscribe(_ => OnInteractButtonClick(button.CatInteractionType)).AddTo(View);
