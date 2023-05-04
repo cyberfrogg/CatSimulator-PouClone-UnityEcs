079e33f8dd8af552c1b208f276b4ef2701076368
cyberfrogg <alexandersudnik.q@gmail.com>
2023-05-04 22:56:31 +0300

    MF - locking buttons

--- Assets/Scripts/Game/Ui/PerformAction/PerformActionView.cs
+++ Assets/Scripts/Game/Ui/PerformAction/PerformActionView.cs
@@ -1,20 +1,44 @@
 using System;
 using System.Collections.Generic;
+using Ecs.Views.Linkable.Impl.Ui;
 using Game.Utils;
 using Game.Utils.Drawers;
-using SimpleUi.Abstracts;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace Game.Ui.PerformAction
 {
-    public class PerformActionView : UiView
+    public class PerformActionView : GameLinkableUiView,
+        ICatInteractionTypeAddedListener
     {
         [KeyValueFormat(nameof(PerformActionViewButtonsPair.CatInteractionType))] 
         [SerializeField] 
         private List<PerformActionViewButtonsPair> buttons;
 
         public List<PerformActionViewButtonsPair> Buttons => buttons;
+        
+        protected override void Listen(GameEntity entity)
+        {
+            entity.AddCatInteractionTypeAddedListener(this);
+        }
+        protected override void Unlisten(GameEntity entity)
+        {
+            entity.RemoveCatInteractionTypeAddedListener(this);
+        }
+        protected override void Reset()
+        {
+            
+        }
+
+        public void OnCatInteractionTypeAdded(GameEntity entity, ECatInteractionType value)
+        {
+            var isButtonsInteractable = value == ECatInteractionType.None;
+
+            foreach (var button in buttons)
+            {
+                button.Button.interactable = isButtonsInteractable;
+            }
+        }
     }
 
     [Serializable]
